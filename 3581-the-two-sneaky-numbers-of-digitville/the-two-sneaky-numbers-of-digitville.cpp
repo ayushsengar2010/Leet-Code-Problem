4307ddc0db08d12b557be14fa5e2c67d7ad2a201
Ayush Sengar <115213301+ayushsengar2010@users.noreply.github.com>
2025-10-31 11:25:00 +0530

    Time: 4 ms (26.50%) | Memory: 26.1 MB (53.66%) - LeetSync

---
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> getSneakyNumbers(vector<int>& nums) {
+        int n = nums.size() - 2;
+        vector<int> freq(n, 0);
+        vector<int> res;
+        for (int num : nums) {
+            freq[num]++;
+            if (freq[num] == 2) {
+                res.push_back(num);
+            }
+        }
+        return res;
+    }
+};
